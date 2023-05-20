4961ef6ebdeca455904c48898e25141c18629662
andulica <75125226+andulica@users.noreply.github.com>
2023-05-20 15:06:32 +0100

    #6 solved by creating a for loop #5 solved by creating constants in the situation where no changes in value would take place #4 solved by creating a constant that is used in other situations too #3 solved by separating UI methods and Logic methods #2 partially solved. User needs to enter "y" or "n" or "exit" to execute different commands while he is playing the game (waiting for further instructions if a better solution exists) #1 build folder removed I appologise for the big commit, it was an unusual situation. Coming back after a long time to this exercise made me improve the game logic too. I will stick to smaller commits from now on

---
+++ GUI.cs
@@ -0,0 +1,115 @@
+﻿namespace SlotMachineExercise
+{
+    internal class GUI
+    {
+        private const int GRID_SIZE = 3;
+        private const int MIN_LINE_TO_PLAY = 0;
+        private const int MAX_LINE_TO_PLAY = 8;
+        private const int ELEMENT_NUMBER = 3;
+
+        public static int[,] populateScreen()
+        {
+            //Populates a 2D array representing the screen of the slotmachine
+            Random rnd = new Random();
+            int[,] screen = new int[GRID_SIZE, GRID_SIZE];
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                for (int j = 0; j < GRID_SIZE; j++)
+                {
+                    screen[i, j] = rnd.Next(1, ELEMENT_NUMBER);
+                }
+            }
+            return screen;
+        }
+
+        //Prints to the console welcome message
+        public static void welcomeMessage()
+        {
+            Console.WriteLine("Welcome to SlotsMachine Ultimate Experience! ");
+        }
+
+        public static int moneyToPlay()
+        {
+            Console.WriteLine("Please enter the ammount of money you want to gamble: ");
+            int moneyToPlay = Convert.ToInt32(Console.ReadLine());
+            return moneyToPlay;
+        }
+
+        //Prints the bet options for user to the console
+        public static void printOptions()
+        {
+            Console.WriteLine(
+                " \n        7            8  " +
+                " \n        '\\'        /   " +
+                " \n 1 --->  | x | x | x |  " +
+                " \n         -------------  " +
+                " \n 2 --->  | x | x | x |  " +
+                " \n         -------------  " +
+                " \n 3 --->  | x | x | x |  " +
+                " \n           ^   ^   ^    " +
+                " \n           |   |   |    " +
+                " \n           4   5   6    ");
+        }
+
+
+        //prints the screen of the slotmachine to the console
+        public static void printScreen(int[,] screen)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    Console.Write(" " + screen[i, k] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //takes user Lines and adds them to a list. If the list is
+        public static List<int> chooseLines()
+        {
+            List<int> chosenLines = new List<int>();
+            int chosenLine = 0;
+
+            bool finish = true;
+            while (finish)
+            {
+                Console.WriteLine("Please select which lines you want to play. " +
+                    "\nEach line is equal with $1 bet and you can keep adding them until you press 'n' or maximum number of lines is reached: ");
+                try
+                {
+                    chosenLine = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    //instead of pressing n just make a function that presses exit or enter
+                    Console.WriteLine("Do you really want to exit the choosing lines process ? If so, press 'y' or press 'n' to continue choosing more lines");
+                    String userChecker = Convert.ToString(Console.ReadLine());
+                    if (userChecker.Equals("y"))
+                    {
+                        finish = false;
+                    }
+                    else
+                        continue;
+                }
+
+                if (chosenLines.Count > MAX_LINE_TO_PLAY || finish == false)
+                {
+                    finish = false;
+                }
+                else if (chosenLine < MIN_LINE_TO_PLAY || chosenLine > MAX_LINE_TO_PLAY || chosenLines.Contains(chosenLine))
+                {
+                    Console.WriteLine($"The line {chosenLine} is not correct, please enter another value");
+                }
+                else
+                {
+                    chosenLines.Add(chosenLine);                   
+
+                }
+            }
+
+            return chosenLines;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,176 +1,70 @@
-﻿
-namespace SlotMachineExercise
+﻿namespace SlotMachineExercise
 {
     internal class Program
     {
-
-        const int MAX_NUMBER_FOR_A_LINE = 8;
-        const int COUNTER_TO_CHECK_HORIZONTAL = 1;
-        const int COUNTER_TO_CHECK_VERTICAL = 4;
-        const int GRID_SIZE = 3;
         static void Main(string[] args)
         {
-            //Populates a 2D array representing the screen of the slotmachine
-            Random rnd = new Random();
-            int[,] screen = new int[GRID_SIZE, GRID_SIZE];
-            for (int i = 0; i < GRID_SIZE; i++)
+            int creditsLeftAfterSpin = 0;
+            List<int> newLinesToPlay = new List<int>();           
+            bool previousSpinSuccessful = false;
+
+            GUI.welcomeMessage();
+            GUI.printOptions();
+            List<int> linesToPlay = new (GUI.chooseLines());
+            int credits = GUI.moneyToPlay();
+            int[,] screen = GUI.populateScreen();
+            bool continueGame = true;
+
+
+            //keeps playing until user decides to quite the game
+            while (continueGame)
             {
-                for (int j = 0; j < GRID_SIZE; j++)
+                if (previousSpinSuccessful == false)
                 {
-                    screen[i, j] = rnd.Next(1, GRID_SIZE);
-                }
-            }
-
-            List<int> listChosenLines = new List<int>();
-
-            Console.WriteLine(
-                " \n        7            8  " +
-                " \n        '\\'        /   " +
-                " \n 1 --->  | x | x | x |  " +
-                " \n         -------------  " +
-                " \n 2 --->  | x | x | x |  " +
-                " \n         -------------  " +
-                " \n 3 --->  | x | x | x |  " +
-                " \n           ^   ^   ^    " +
-                " \n           |   |   |    " +
-                " \n           4   5   6    ");
-
-            Console.WriteLine("Please select how much you want to gamble: ");
-            int gambleSum = Convert.ToInt32(Console.ReadLine());
-
-            bool start = false;
-            //loops until player decides to start. He needs to choose atleast 1 line
-            while (!start)
-            {
-                Console.WriteLine("Each line is $1 bet. Please enter the coresponding number for each line that you want to play: ");
-
-                int chosenLine = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine();
-
-                if (chosenLine <= MAX_NUMBER_FOR_A_LINE && chosenLine > 0 && !listChosenLines.Contains(chosenLine))
-                {
-                    listChosenLines.Add(chosenLine);
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a number from 1 to 8!" +
-                        "\nAlso no repeating lines allowed! ");
+                    creditsLeftAfterSpin = GameLogic.allLinesChecker(linesToPlay, credits, screen);
+                    GUI.printScreen(screen);
                 }
 
-                Console.WriteLine("If you want to enter more lines, press Enter. If not , enter Start: ");
-                string checkUser = Convert.ToString(Console.ReadLine().ToLower());
+                Console.WriteLine("If you wish to continue with the same gamble, please press 'y.' \nAlternatively, if you prefer to modify your playing values, press 'n'.\n To exit the game type 'exit'");
+                String userChecker = Convert.ToString(Console.ReadLine().ToLower());
 
-                if (checkUser == "start" && listChosenLines.Count >= 1)
+
+                if (userChecker.Equals("n"))
                 {
-                    start = true;
-                }
-            }
-
-            printArray(screen);
-
-            int sumWon = 0;
-
-            Console.WriteLine("The lines you chose to play are:  ");
-            foreach (int line in listChosenLines)
-            {
-                Console.Write($" {line} ");
-            }
-            Console.WriteLine();
-
-
-
-            for (int i = 0; i < listChosenLines.Count; i++)
-            {
-                if (gambleSum >= listChosenLines.Count)
-                {
-                    if (listChosenLines[i] <= 3)
+                    newLinesToPlay = new(GUI.chooseLines());
+                    if (creditsLeftAfterSpin < newLinesToPlay.Count)
                     {
-                        sumWon += checkHorizontal(screen, listChosenLines[i] - COUNTER_TO_CHECK_HORIZONTAL);
+                        creditsLeftAfterSpin = +GUI.moneyToPlay();
+                    }
+                    int[,] newScreen = GUI.populateScreen();
+                    creditsLeftAfterSpin = GameLogic.allLinesChecker(newLinesToPlay, creditsLeftAfterSpin, newScreen);
+                    GUI.printScreen(newScreen);
+                    previousSpinSuccessful = true;
+
+                }
+                else if (userChecker.Equals("y"))
+                {
+                    if (newLinesToPlay.Count == 0)
+                    {
+                        newLinesToPlay = linesToPlay;
                     }
 
-                    if (listChosenLines[i] > 3 && listChosenLines[i] <= 6)
+                    if (newLinesToPlay.Count > creditsLeftAfterSpin)
                     {
-                        sumWon += checkVertical(screen, listChosenLines[i] - COUNTER_TO_CHECK_VERTICAL);
+                        creditsLeftAfterSpin = +GUI.moneyToPlay();
                     }
-
-                    if (listChosenLines[i] == 7)
+                    else
                     {
-                        sumWon += checkDiagonal1(screen);
-                    }
+                        int[,] generateNewScreen = GUI.populateScreen();
+                        creditsLeftAfterSpin = GameLogic.allLinesChecker(newLinesToPlay, creditsLeftAfterSpin, generateNewScreen);
+                        GUI.printScreen(generateNewScreen);
+                        previousSpinSuccessful = true;
 
-                    if (listChosenLines[i] == 8)
-                    {
-                        sumWon += checkDiagonal2(screen);
                     }
                 }
-                else
-                {
-                    Console.WriteLine("Insuficient funds!");
-                }
+                else if (userChecker.Equals("exit"))
+                    continueGame = false;               
             }
-
-            Console.WriteLine($"You have won in total:{sumWon} USD! ");
-
-            int totalSum = (gambleSum - listChosenLines.Count) + sumWon;
-
-            Console.WriteLine($"Total funds left = {totalSum} USD! ");
-        }
-
-
-        //prints the screen of the slotmachine to the console
-        public static void printArray(int[,] twoDArray)
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int k = 0; k < 3; k++)
-                {
-                    Console.Write(" " + twoDArray[i, k] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        //Checks if player won on horizontal lines
-        public static int checkHorizontal(int[,] twoDArray, int lineToCheck)
-        {
-            if (twoDArray[lineToCheck, 0] == twoDArray[lineToCheck, 1] && twoDArray[lineToCheck, 0] == twoDArray[lineToCheck, 2])
-            {
-                return 1;
-            }
-            return 0;
-        }
-
-        //Checks if player won on vertical lines
-        public static int checkVertical(int[,] twoDArray, int lineToCheck)
-        {
-            if (twoDArray[0, lineToCheck] == twoDArray[1, lineToCheck] && twoDArray[0, lineToCheck] == twoDArray[2, lineToCheck])
-            {
-                Console.WriteLine("This is vertical");
-                return 1;
-            }
-            return 0;
-        }
-
-        //Checks if player won on diagonal from top left corner to bottom right corner
-        public static int checkDiagonal1(int[,] twoDArray)
-        {
-            if (twoDArray[0, 0] == twoDArray[1, 1] && twoDArray[0, 0] == twoDArray[2, 2])
-            {
-                Console.WriteLine("This is diagonal");
-                return 1;
-            }
-            return 0;
-        }
-
-        //Checks if player won on diagonal from top right corner to bottom left corner
-        public static int checkDiagonal2(int[,] twoDArray)
-        {
-            if (twoDArray[0, 2] == twoDArray[1, 1] && twoDArray[0, 2] == twoDArray[2, 0])
-            {
-                Console.WriteLine("This is diagonal2");
-                return 1;
-            }
-            return 0;
-        }
+        }   
     }
 }
